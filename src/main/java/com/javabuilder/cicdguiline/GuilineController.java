8c0d5ff644bfd49aa25e87fa0fa6b82afcacb347
lekhanhduc <lekhanhduccc@gmail.com>
2026-05-09 15:41:40 +0700

    add GuilineController with basic /guiline endpoint

---
+++ src/main/java/com/javabuilder/cicdguiline/GuilineController.java
@@ -0,0 +1,13 @@
+package com.javabuilder.cicdguiline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GuilineController {
+
+    @GetMapping("/guiline")
+    public String guiline() {
+        return "Continues Integration";
+    }
+}
