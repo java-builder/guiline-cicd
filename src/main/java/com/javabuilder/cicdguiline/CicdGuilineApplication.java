665c1ba3ceecf81f248162b8f37a3647d461ce02
lekhanhduc <lekhanhduccc@gmail.com>
2026-05-09 15:37:54 +0700

    ci pipeline

---
+++ src/main/java/com/javabuilder/cicdguiline/CicdGuilineApplication.java
@@ -0,0 +1,13 @@
+package com.javabuilder.cicdguiline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdGuilineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdGuilineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javabuilder/cicdguiline/CicdGuilineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabuilder.cicdguiline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdGuilineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
